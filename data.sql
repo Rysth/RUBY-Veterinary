832c660e3d472da4e635a21b2438e601c4df66c3
Rysth <johnpalacios.t@gmail.com>
2023-09-21 09:56:41 -0500

    Insert data into vets, and specializations tables.

--- data.sql
+++ data.sql
@@ -74,4 +74,30 @@ SET owners_id = CASE
                  WHEN name IN ('Angemon', 'Boarmon') THEN 5
                END;
 
+/* PROJECT PART #4 */
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vet_id, species_id)
+VALUES
+  (
+    (SELECT id FROM vets WHERE name = 'William Tatcher'),
+    (SELECT id FROM species WHERE name = 'Pokemon')
+  ),
+  (
+    (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Digimon')
+  ),
+  (
+    (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+    (SELECT id FROM species WHERE name = 'Pokemon')
+  ),
+  (
+    (SELECT id FROM vets WHERE name = 'Jack Harkness'),
+    (SELECT id FROM species WHERE name = 'Digimon')
+  );
 
