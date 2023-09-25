79ab2caedcf21e2b2888d00bd05480609e404479
SnakyMz <Soban.syeed@outlook.com>
2023-09-26 00:42:24 +0530

    Update queries

--- queries.sql
+++ queries.sql
@@ -270,33 +270,14 @@ GROUP BY Specialty
 ORDER BY VisitCount DESC
 LIMIT 1;
 
-/* Preparation - Week 2 */
-
--- Add an email column to your owners table
-ALTER TABLE owners ADD COLUMN email VARCHAR(120);
-
--- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
-INSERT INTO visits (animal_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
-
--- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-INSERT INTO owners (full_name, age, email) SELECT 'Owner ' || generate_series(1, 2500000), generate_series(1, 2500000) + 20, 'owner_' || generate_series(1, 2500000) || '@mail.com';
-
 /* Project Requeriments - Week 2 */
 
+/* Querying before optimization */ 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
-/* 
-    Optimized
-    - Created an INDEX for the animals ID help us to optimize A LOT the performance of the query.
-    - Avoided the aggregate function COUNT(*) to reduce the amount of execution time. Because it's not needed now.
- */
-CREATE INDEX idx_animal_id ON visits (animal_id);
-EXPLAIN ANALYZE SELECT * FROM visits where animal_id = 4;
-
-CREATE INDEX idx_vets_id ON visits (vets_id);
+/* Querying after optimization */
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
-
-CREATE INDEX idx_email ON owners (email);
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
